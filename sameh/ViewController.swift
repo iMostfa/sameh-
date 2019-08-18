bf8fc97d32e3a0fb42bf6aa368339e6cf64a8309
Mostfa <almme111@gmail.com>
2019-08-18 02:55:58 +0200

    Initial Commit

---
+++ sameh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sameh
+//
+//  Created by mostfa on 8/18/19.
+//  Copyright © 2019 mostfa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ samehTests/samehTests.swift
@@ -0,0 +1,34 @@
+//
+//  samehTests.swift
+//  samehTests
+//
+//  Created by mostfa on 8/18/19.
+//  Copyright © 2019 mostfa. All rights reserved.
+//
+
+import XCTest
+@testable import sameh
+
+class samehTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
